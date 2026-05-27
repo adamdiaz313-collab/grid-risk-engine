8d757a9adc3c7ca4bbdcf19c8e2336180b093a7b
Adam Diaz <adam.diaz313@gmail.com>
2026-05-27 10:38:54 -0400

    Update App.jsx

--- Frontend - Copy/src/App.jsx
+++ Frontend - Copy/src/App.jsx
@@ -130,7 +130,7 @@ export default function App() {
       setError("");
       setResult(null);
 
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://grid-risk-engine-1.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
